d215985beec776da6f1ef1383c3b6c14a6161511
Lem Apperson <lemapp09@gmail.com>
2025-09-24 09:09:34 -0400

    Added Plants

--- Library/PackageCache/com.unity.purchasing@821fd0bcef6f/Plugins/UnityPurchasing/iOS/SK2/Models/ProductDefinition.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public struct ProductDefinition: Decodable {
-    let id: String
-    let storeSpecificId: String
-    let type: String
-    let enabled: Bool
-    let payouts: [String]?
-}
--- Library/PackageCache/com.unity.purchasing@821fd0bcef6f/Plugins/UnityPurchasing/iOS/SK2/Responses/ResponseProtocol.swift
+++
@@ -1,5 +0,0 @@
-/**
- This protocol provides interface for any responses from the StoreKits API call
- */
-public protocol ResponseProtocol {
-}
